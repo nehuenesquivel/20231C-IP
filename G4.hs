91a089263f0ae8de8e2ad1d90be5182e28c38f74
nehuenesquivel <nehuenesquivel@outlook.com>
2023-04-19 10:22:49 -0300

    improvement^1

--- G4.hs
+++ G4.hs
@@ -1,27 +1,49 @@
 --1
 fibonacci :: Integer -> Integer
+fibonacci n | n < 2 = n
+            | otherwise = fibonacci(n - 1) + fibonacci(n - 2)
+
+{-
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
-
-{-
-fibonacci n | n == 0 = 0
-            | n == 1 = 0
-            | otherwise = fibonacci(n - 1) + fibonacci(n - 2)
 -}
 
 --2
 parteEntera :: Float -> Integer
-parteEntera x = floor(x)
+parteEntera x | x <= -1 = parteEntera(x + 1) - 1
+              | x < 0 = -1
+              | x >= 1 = parteEntera(x - 1) + 1
+              | otherwise = 0
+
+{-
+esPositivo :: Float -> Integer
+esPositivo x | x > 0 = esPositivo(x - 1) + 1
+             | otherwise = (-1)
+
+esNegativo :: Float -> Integer
+esNegativo x | x < 0 = esNegativo(x + 1) - 1
+             | otherwise = 1
+-}
 
 --3
 esDivisible :: Integer -> Integer -> Bool
-esDivisible a b = a == b || (b < a && True)
+esDivisible a b | a > b = esDivisible (a - b) b
+                | otherwise = a == b
 
 --4
 sumaImpares :: Integer -> Integer
 sumaImpares 1 = 1
 sumaImpares n = sumaImpares(n - 1) + n * 2 - 1
+
+--5
+-- medioFact :: Integer -> Integer
+-- medioFact 1 = 1
+-- medioFact n = (n - 2) * 
+
+--6
+-- sumaDigitos :: Integer -> Integer
+-- sumaDigitos n = mod n 10
 
 {-noname :: Integer -> Integer
 noname n | n * 2 - 1 + (n)
