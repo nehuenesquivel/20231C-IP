7906459479ce269fd5724477526cecdad69fe59e
nehuenesquivel <nehuenesquivel@outlook.com>
2023-04-26 00:22:25 -0300

    G5 vibing

--- G5.hs
+++ G5.hs
@@ -1,16 +1,27 @@
 --1
 longitud :: [t] -> Integer
 longitud [] = 0
-longitud s = longitud (tail s) + 1
+longitud (_:t) = longitud t + 1
 
 ultimo :: [t] -> t
 ultimo [e] = e
-ultimo s = ultimo (tail s)
+ultimo (_:t) = ultimo t
 
 principio :: [t] -> [t]
 principio [e] = []
-principio s = head s : principio (tail s)
+principio (h:t) = h : principio t
 
 reverso :: [t] -> [t]
 reverso [] = []
-reverso s = ultimo s : reverso (principio s)
+reverso (h:t) = reverso t ++ [h] -- = ultimo s : reverso (principio s)
+-- hacer de la tercer manera o pensarla al menos (sin ++ y recursiva?)
+
+--2
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece e (h:t) = e == h || pertenece e t
+
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = False
+todosIguales [e] = True
+todosIguales (h:t) = h == head t && todosIguales t
