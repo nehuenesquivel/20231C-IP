d036706b57d99e311f674e9d05eaace84c671ba6
nehuenesquivel <nehuenesquivel@outlook.com>
2023-05-24 12:16:48 -0300

    E2 ej2 done

--- E2/fibonacciNoRecursivo.py
+++ E2/fibonacciNoRecursivo.py
@@ -1,9 +1,19 @@
 import sys
 
 def fibonacciNoRecursivo(n: int) -> int:
-  # Implementar esta funcion
-  return 0
-
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1  
+  else:
+    fibonacciN: int = 0
+    fibonacciN2: int = 0
+    fibonacciN1: int = 1
+    for i in range(2, n + 1):
+      fibonacciN = fibonacciN1 + fibonacciN2
+      fibonacciN2 = fibonacciN1
+      fibonacciN1 = fibonacciN
+    return fibonacciN
 
 if __name__ == '__main__':
   x = int(input())
