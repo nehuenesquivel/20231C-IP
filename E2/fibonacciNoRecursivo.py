3f643ebcdcd206504c7f226780f6900120637bdc
nehuenesquivel <nehuenesquivel@outlook.com>
2023-05-24 22:08:33 -0300

    E2 ej3 done

--- E2/fibonacciNoRecursivo.py
+++ E2/fibonacciNoRecursivo.py
@@ -1,17 +1,17 @@
 import sys
 
 def fibonacciNoRecursivo(n: int) -> int:
-  fibonacciN: int = 0
+  resultado: int = 0
   if n < 2:
-    fibonacciN = n
+    resultado = n
   else:
     fibonacciN2: int = 0
     fibonacciN1: int = 1
     for i in range(2, n + 1):
-      fibonacciN = fibonacciN1 + fibonacciN2
+      resultado = fibonacciN1 + fibonacciN2
       fibonacciN2 = fibonacciN1
-      fibonacciN1 = fibonacciN
-  return fibonacciN
+      fibonacciN1 = resultado
+  return resultado
 
 if __name__ == '__main__':
   x = int(input())
--- E2/mesetaMasLarga.py
+++ E2/mesetaMasLarga.py
@@ -1,27 +1,16 @@
 from typing import List
 
 def mesetaMasLarga(l: List[int]) -> int :
-  longitudMeseta: int = 0
-  longitudLista: int = len(l)
-  if longitudLista < 2:
-    longitudMeseta = longitudLista
-  else:
-    longitud: int = 2
-    for i in range(2, longitudLista):
-        while(l[i] == l[i - 1]):
-          longitud += 1
-          i += 1
-        longitudMeseta = max(longitudMeseta, longitud) 
+  longitud: int = len(l)
+  resultado: int = 0
+  j: int = 0
+  for i in range(0, longitud):
+    while(i + 1 < longitud and l[i] == l[i + 1]):
+      i += 1
+    resultado = max(resultado, i - j + 1)
+    j = i + 1
+  return resultado
 
-
-      # if (l[i] != l[i - 1] or i + 1 == longitudLista):
-      #   longitudMeseta = max(longitudMeseta, i + 1 - inicio)
-      #   inicio = i + 1
-
-    # if (l[i] != l[i + 1] or i == len(l) - 2):
-    #   longitud = i + 1 - inicio
-    #   inicio = i + 1
-  return longitudMeseta
 if __name__ == '__main__':
   x = input()
   print(mesetaMasLarga([int(j) for j in x.split()]))
