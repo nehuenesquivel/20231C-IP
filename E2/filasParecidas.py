99249ddfd471ead75d6138b411cf1c24b632f767
nehuenesquivel <nehuenesquivel@outlook.com>
2023-05-25 12:31:04 -0300

    E2 ej4 done

--- E2/filasParecidas.py
+++ E2/filasParecidas.py
@@ -1,12 +1,13 @@
 from typing import List
 
-# Aclaración: Debido a la versión de Python del CMS, para el tipo Lista, la sintaxis de la definición de tipos que deben usar es la siguiente:
-# l: List[int]  <--Este es un ejemplo para una lista de enteros.
-# Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
-def filasParecidas(matriz: List[List[int]]) -> bool :
-  # Implementar esta funcion
+def filasParecidas(matriz: List[List[int]]) -> bool:
+  if len(matriz) > 1:
+    n: int = matriz[1][0] - matriz[0][0]
+    for i in range(1, len(matriz)):
+      for j in range(0, len(matriz[i])):
+        if matriz[i][j] - matriz[i - 1][j] != n:
+          return False
   return True
-
 
 if __name__ == '__main__':
   filas = int(input())
