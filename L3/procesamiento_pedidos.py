84891adbd2e8433e31c7c7013071d0a420a07c80
nehuenesquivel <nehuenesquivel@outlook.com>
2023-06-17 21:16:42 -0300

    90/100

--- L3/procesamiento_pedidos.py
+++ L3/procesamiento_pedidos.py
@@ -4,31 +4,28 @@ from typing import Dict
 from typing import Union
 import json
 
-def estado_incompleto(pedido: dict[str, Union[int, str, dict[str, int]]]):
-  if pedido["estado"] == "completo":
-    pedido["estado"] = "incompleto"
-
 def procesamiento_pedidos(pedidos: Queue, stock_productos: Dict[str, int], precios_productos: Dict[str, float]) -> List[Dict[str, Union[int, str, float, Dict[str, int]]]]:
-  resultado: list[dict[str, Union[int, str, dict[str, int]]]] = []
-  pedido: dict[str, Union[int, str, dict[str, int]]] = {}
+  resultado = []
+  pedido = {}
   nombre_producto: str = ""
   cantidad_producto: int = 0
   cantidad_disponible: int = 0
 
   while not pedidos.empty():
     pedido = pedidos.get()
+
     pedido["precio_total"] = 0
-    pedido["estado"] = "completo"
+      pedido["estado"] = "completo"
 
     for producto in pedido["productos"].items():
       nombre_producto = producto[0]
       cantidad_producto = producto[1]
-
       if stock_productos[nombre_producto] > 0:
 
         if stock_productos[nombre_producto] - cantidad_producto < 0:
           cantidad_disponible = stock_productos[nombre_producto]
-          estado_incompleto(pedido)
+          if pedido["estado"] == "completo":
+            pedido["estado"] = "incompleto"
 
         else:
           cantidad_disponible = cantidad_producto
@@ -39,7 +36,8 @@ def procesamiento_pedidos(pedidos: Queue, stock_productos: Dict[str, int], preci
 
       else:        
         pedido["productos"][nombre_producto] = 0
-        estado_incompleto(pedido)
+        if pedido["estado"] == "completo":
+          pedido["estado"] = "incompleto"
     
     resultado.append(pedido)
 
@@ -52,10 +50,3 @@ if __name__ == '__main__':
   stock_productos = json.loads(input())
   precios_productos = json.loads(input())
   print("{} {}".format(procesamiento_pedidos(pedidos, stock_productos, precios_productos), stock_productos))
-
-pedidos: Queue = Queue()
-lista_pedidos = json.loads(input())
-[pedidos.put(p) for p in lista_pedidos]
-stock_productos = json.loads(input())
-precios_productos = json.loads(input())
-print("{} {}".format(procesamiento_pedidos(pedidos, stock_productos, precios_productos), stock_productos))
