b54da218db2e3bdb30b66ed01712fc418b3c5dcd
nehuenesquivel <nehuenesquivel@outlook.com>
2023-06-17 22:49:53 -0300

    ej4 100/100

--- L3/Fila_Del_Banco.py
+++ L3/Fila_Del_Banco.py
@@ -1,8 +1,51 @@
 from queue import Queue
 
-# El tipo de fila debería ser Queue[int], pero la versión de python del CMS no lo soporta. Usaremos en su lugar simplemente "Queue"
 def avanzarFila(fila: Queue, min: int):
-  #implementar función
+  nueva_persona = fila.qsize() + 1
+  persona_problematica = 0
+
+  for minuto in range(0, min + 1):
+
+    #final de la fila
+    if persona_problematica != 0 and (minuto - 3) % 4 == 2:
+      # print(str(minuto) + "persona problematica")
+      fila.put(persona_problematica)
+      persona_problematica = 0      
+      
+    #nueva persona
+    if minuto % 4 == 0:
+      # print(str(minuto) + "nueva persona")
+      fila.put(nueva_persona)
+      nueva_persona += 1
+      
+    #caja1
+    if minuto % 10 == 1:
+      # print(str(minuto) + "caja1")
+      if not fila.empty():
+        fila.get()
+
+    #caja2
+    if minuto % 4 == 3:
+      # print(str(minuto) + "caja2")
+      if not fila.empty():
+        fila.get()
+    
+    #caja3
+    if minuto % 4 == 2:
+      # print(str(minuto) + "caja3")
+      if not fila.empty():
+        persona_problematica = fila.get()
+
+  return fila
+
+# q = Queue()
+# for e in [1, 2, 3]:
+#   q.put(e)
+# avanzarFila(q, 4)
+# l = []
+# while not q.empty():
+#   l.append(q.get())
+# print(l)
 
 if __name__ == '__main__':
   fila: Queue = Queue()
